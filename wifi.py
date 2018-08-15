345475f12d50748df9b7820e8ab51069ba8551d5
abhimanyu <abhimanyukrsingh00@gmail.com>
2018-08-15 15:25:28 +0530

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -4,5 +4,5 @@ driver =webdriver.Firefox()
 
 driver.get("https://scg.ruckuswireless.com:9998/SubscriberPortal/login")
 driver.execute_script('document.getElementById("UE-Username").value="CS16B1001";document.getElementById("UE-Password").value="your password";document.forms["loginForm"].submit();')
-time.sleep(2)
+time.sleep(4)
 driver.quit()
